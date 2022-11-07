e27c450bf3c7b08bbb72dc2f1f34a098e3b22776
cameronmcewan <cm17521@bristol.ac.uk>
2022-11-07 09:40:02 +0000

    remove wallet info from navbar

--- src/components/elements/navigation/NavBar/paths.ts
+++ src/components/elements/navigation/NavBar/paths.ts
@@ -8,42 +8,42 @@ const NAV_LINKS: ISubNav[] = [
     label: 'Forums',
     href: '/forums',
   },
-  {
-    label: 'Your Wallet',
-    href: '/balances',
-    children: [
-      {
-        label: 'ERC20 Balances',
-        subLabel: 'Get your ERC20 balances',
-        href: '/balances/erc20',
-        logo: 'token',
-      },
-      {
-        label: 'ERC20 Transfers',
-        subLabel: 'Get your ERC20 transfers',
-        href: '/transfers/erc20',
-        logo: 'chest',
-      },
-      {
-        label: 'NFT Balances',
-        subLabel: 'Get your ERC721 and ERC1155 balances',
-        href: '/balances/nft',
-        logo: 'pack',
-      },
-      {
-        label: 'NFT Transfers',
-        subLabel: 'Get your ERC721 and ERC1155 transfers',
-        href: '/transfers/nft',
-        logo: 'lazyNft',
-      },
-      {
-        label: 'Transactions',
-        subLabel: 'Get all your transactions',
-        href: '/transactions',
-        logo: 'marketplace',
-      },
-    ],
-  },
+  // {
+  //   label: 'Your Wallet',
+  //   href: '/balances',
+  //   children: [
+  //     {
+  //       label: 'ERC20 Balances',
+  //       subLabel: 'Get your ERC20 balances',
+  //       href: '/balances/erc20',
+  //       logo: 'token',
+  //     },
+  //     {
+  //       label: 'ERC20 Transfers',
+  //       subLabel: 'Get your ERC20 transfers',
+  //       href: '/transfers/erc20',
+  //       logo: 'chest',
+  //     },
+  //     {
+  //       label: 'NFT Balances',
+  //       subLabel: 'Get your ERC721 and ERC1155 balances',
+  //       href: '/balances/nft',
+  //       logo: 'pack',
+  //     },
+  //     {
+  //       label: 'NFT Transfers',
+  //       subLabel: 'Get your ERC721 and ERC1155 transfers',
+  //       href: '/transfers/nft',
+  //       logo: 'lazyNft',
+  //     },
+  //     {
+  //       label: 'Transactions',
+  //       subLabel: 'Get all your transactions',
+  //       href: '/transactions',
+  //       logo: 'marketplace',
+  //     },
+  //   ],
+  // },
   {
     label: 'DAO',
     href: '/dao/',
